572a657b80d3191abde5960c339eb69ae2998b36
Mzar10 <joharivoladylan@gmail.com>
2025-07-08 09:36:36 +0300

    commit

--- admin/fairePret.php
+++ admin/fairePret.php
@@ -386,7 +386,7 @@ include 'sidebar.php';
     </div>
 
     <script>
-        const apiBase = "http://localhost/serveur/S4/WEB_S4_MVC/ws";
+        const apiBase = "http://localhost/WEB_S4_MVC/ws";
 
         function ajax(method, url, data, callback) {
             const xhr = new XMLHttpRequest();
