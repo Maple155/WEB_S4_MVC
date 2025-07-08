b6583fb38411d21056bc4281a8c1f94f92df9256
Maple155 <rantoluciano622@gmail.com>
2025-07-08 11:27:16 +0300

    Update db.php

--- ws/db.php
+++ ws/db.php
@@ -5,6 +5,11 @@ function getDB() {
     $username = 'root';
     $password = 'root';
 
+    // $host = '172.160.0.13';
+    // $dbname = 'db_s2_ETU003113';
+    // $usernamer = 'ETU003113';
+    // $password = 'q3fZmo1u';
+
     try {
         return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
